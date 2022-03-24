175341327364afb19067b407be70e92fcdad8f8d
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-24 23:06:55 +0300

    Update comment.py

--- data/comment.py
+++ data/comment.py
@@ -9,12 +9,12 @@ class Comment(SqlAlchemyBase, UserMixin):
     __tablename__ = "comments"
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-
+    # комментарий
     comment = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # дата внесения в базу данных
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-
+    # создатель комментария (user)
     creator = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relation('User')
