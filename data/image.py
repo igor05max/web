944f383acc14e19e5dd71b0228c04bd06418e406
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-24 23:09:48 +0300

    Update image.py

--- data/image.py
+++ data/image.py
@@ -9,12 +9,12 @@ class Image(SqlAlchemyBase, UserMixin):
     __tablename__ = "images"
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-
+    # картинка
     image = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # дата внесения в базу данных
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-
+    # создатель картинки
     creator = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relation('User')
