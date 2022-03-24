c8a5395d9b6534a0af818de22fe563b816eea6ea
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-24 23:04:10 +0300

    Update city.py

--- data/city.py
+++ data/city.py
@@ -8,9 +8,10 @@ class City(SqlAlchemyBase, UserMixin):
     __tablename__ = "cities"
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
+    # название города
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # список достопримечательностей
     attractions = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # дата внесения в базу данных
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
