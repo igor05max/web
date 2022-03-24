9fa1a522014e7e835f2af170256d22549ea0a9c8
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-24 23:01:58 +0300

    Update attraction.py

--- data/attraction.py
+++ data/attraction.py
@@ -8,14 +8,14 @@ class Attraction(SqlAlchemyBase, UserMixin):
     __tablename__ = "attractions"
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # название достопримечательности
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)  
+    # список картинок достопримечательности
     img = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # категория (памятник, музей, парк и т.д)
     category = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # список комментариев
     comments = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
+    # дата внесения в базу данных
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
