27f2a49c57d8848702ff84b038cc9996a53e9c44
igor05max <94913244+igor05max@users.noreply.github.com>
2022-03-25 21:02:53 +0300

    Add files via upload

---
+++ data/location.py
@@ -0,0 +1,20 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+
+
+class Location(SqlAlchemyBase, UserMixin):
+    __tablename__ = "locations"
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)  # название достопримечательности
+    # список картинок достопримечательности
+    img = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    # категория (памятник, музей, парк и т.д)
+    category = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    # список комментариев
+    comments = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    # дата внесения в базу данных
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
