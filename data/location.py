f16d84348e26b8d1c216a36d5da025ed3787333e
igor05max <94913244+igor05max@users.noreply.github.com>
2022-04-15 21:39:25 +0300

    Update location.py

--- data/location.py
+++ data/location.py
@@ -1,11 +1,12 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 from flask_login import UserMixin
 from sqlalchemy import orm
 
 
-class Location(SqlAlchemyBase, UserMixin):
+class Location(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = "locations"
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
@@ -18,6 +19,14 @@ class Location(SqlAlchemyBase, UserMixin):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    count_visits = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    # создатель комментария (user)
+    creator = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+
     city_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                    sqlalchemy.ForeignKey("cities.id"))
+                                sqlalchemy.ForeignKey("cities.id"))
     city = orm.relation('City')
