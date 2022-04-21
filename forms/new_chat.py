f22d9cf24a9a5fbc8b71bf2ef219513e32271990
igor05max <94913244+igor05max@users.noreply.github.com>
2022-04-22 00:09:54 +0300

    Update new_chat.py

--- forms/new_chat.py
+++ forms/new_chat.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms import TextAreaField, SubmitField
 
 
+# начать общение
 class NewChat(FlaskForm):
     message = TextAreaField("Сообщение")
     submit = SubmitField('Начать общение')
