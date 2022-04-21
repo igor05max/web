2b29012d4991c2ae8fa7ed126a7a1ffd497f96af
igor05max <94913244+igor05max@users.noreply.github.com>
2022-04-22 00:09:14 +0300

    Update location.py

--- forms/location.py
+++ forms/location.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField, MultipleFileField
-from wtforms.fields import EmailField
+from wtforms import StringField, TextAreaField, SubmitField, MultipleFileField
 from wtforms.validators import DataRequired
 
 
+# создание локации и редактирование
 class LocationForm(FlaskForm):
     file = MultipleFileField("")
-    name_location = StringField('Название места', validators=[DataRequired()])
+    name_location = StringField('Название', validators=[DataRequired()])
     city = StringField('autocomplete_input', validators=[DataRequired()])
+    category = StringField('Категория (памятник, музей, парк и т.д)', validators=[DataRequired()])
     about = TextAreaField("О месте")
     submit = SubmitField('Создать')
