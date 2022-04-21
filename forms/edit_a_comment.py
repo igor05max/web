0455a31d2f07a91b7a54560981ff28e05beb9288
igor05max <94913244+igor05max@users.noreply.github.com>
2022-04-22 00:07:41 +0300

    Update edit_a_comment.py

--- forms/edit_a_comment.py
+++ forms/edit_a_comment.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
-from wtforms.fields import EmailField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField
 
 
+# редактирование комментария
 class EditAComment(FlaskForm):
     comment = TextAreaField("Комментарий")
     submit = SubmitField('Редактировать')
